c99ab4f900e6bb4b650f540aa156060c62f00e73
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2015-10-09 18:46:44 +0530

    Added two buttons

--- AndroidApp/BT_SignalSender/app/src/main/java/com/example/arjun/bt_signalsender/GenericSenderFragment.java
+++ AndroidApp/BT_SignalSender/app/src/main/java/com/example/arjun/bt_signalsender/GenericSenderFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Fragment2.OnFragmentInteractionListener} interface
+ * {@link GenericSenderFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Fragment2#newInstance} factory method to
+ * Use the {@link GenericSenderFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment2 extends Fragment {
+public class GenericSenderFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -35,11 +35,11 @@ public class Fragment2 extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment2.
+     * @return A new instance of fragment GenericSenderFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Fragment2 newInstance(String param1, String param2) {
-        Fragment2 fragment = new Fragment2();
+    public static GenericSenderFragment newInstance(String param1, String param2) {
+        GenericSenderFragment fragment = new GenericSenderFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -47,7 +47,7 @@ public class Fragment2 extends Fragment {
         return fragment;
     }
 
-    public Fragment2() {
+    public GenericSenderFragment() {
         // Required empty public constructor
     }
 
@@ -64,7 +64,7 @@ public class Fragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+        return inflater.inflate(R.layout.generic_sender, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
