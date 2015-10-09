9edc2f4af12961301d02ac1e398af997c1266ef2
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2015-10-09 21:45:45 +0530

    Minor changes

--- AndroidApp/BT_SignalSender/app/src/main/java/com/example/arjun/bt_signalsender/MainActivityFragment.java
+++ AndroidApp/BT_SignalSender/app/src/main/java/com/example/arjun/bt_signalsender/MainActivityFragment.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-
+    
     public MainActivityFragment() {
     }
 
