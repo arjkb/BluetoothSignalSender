66fc804f62dc19be347fd9ea4318bbbbd49d1136
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2015-10-10 11:04:51 +0530

    Removed unused import

--- AndroidApp/BT_SignalSender/app/src/main/java/com/example/arjun/bt_signalsender/MainActivity.java
+++ AndroidApp/BT_SignalSender/app/src/main/java/com/example/arjun/bt_signalsender/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
