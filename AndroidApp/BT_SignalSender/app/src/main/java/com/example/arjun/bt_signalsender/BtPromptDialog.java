37fb50e626eff4e2e1b3f7df61ae1c99eadc1e09
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2015-10-11 21:07:32 +0530

    Builds prompt. Displays title, text and buttons. Stub for button-clicks

--- AndroidApp/BT_SignalSender/app/src/main/java/com/example/arjun/bt_signalsender/BtPromptDialog.java
+++ AndroidApp/BT_SignalSender/app/src/main/java/com/example/arjun/bt_signalsender/BtPromptDialog.java
@@ -1,7 +1,42 @@
 package com.example.arjun.bt_signalsender;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+
 /**
  * Created by arjun on 11/10/15.
  */
-public class BtPromptDialog {
+public class BtPromptDialog extends DialogFragment {
+    final String MYLOGTAG = "BT_SignalSender";
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        Log.v(MYLOGTAG, "BtPromptDialog: onCreateDialog()");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setMessage(R.string.btprompt_message)
+                .setTitle(R.string.btprompt_title)
+                .setPositiveButton(R.string.btprompt_positive, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.v(MYLOGTAG, "BtPromptDialog: Positive button clicked!");
+
+                    }
+                })
+                .setNegativeButton(R.string.btprompt_negative, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.v(MYLOGTAG, "BtPromptDialog: Negative button clicked!");
+                    }
+                });
+        //return super.onCreateDialog(savedInstanceState);
+        return builder.create();
+    }
 }
+
